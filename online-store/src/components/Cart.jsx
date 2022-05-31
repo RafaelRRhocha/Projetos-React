46222634960ef1142310ea952aa69f8dcedb00b1
rafael rocha <1rafaelrocha2@gmail.com>
2022-05-31 18:41:32 -0300

    update favicon

--- online-store/src/components/Cart.jsx
+++ online-store/src/components/Cart.jsx
@@ -41,15 +41,15 @@ export default class Cart extends React.Component {
   render() {
     const { cart } = this.state;
     return (
-      <div>
+      <div className="flex flex-col items-center gap-5 m-7">
         {cart === null ? (
           <p data-testid="shopping-cart-empty-message">
             Seu carrinho está vazio
           </p>
         ) : (
           cart?.map((items, index) => (
-            <div key={ index }>
-              <div className="flex items-center gap-1">
+            <div key={ index } className="flex justify-center mt-5">
+              <div className="flex items-center gap-4">
                 <img className="w-[150px]" src={ items.thumbnail } alt="produto do carrinho" />
                 <p data-testid="shopping-cart-product-name">{items.title}</p>
                 <div className="flex flex-col">
@@ -75,7 +75,7 @@ export default class Cart extends React.Component {
             </div>
           ))
         )}
-        <Link data-testid="checkout-products" to="/checkout">
+        <Link data-testid="checkout-products" to="/checkout" className="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">
           Finalizar Compra
         </Link>
       </div>
