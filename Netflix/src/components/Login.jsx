9aec201b1246d10fbc4d48e5ecc4f9448e3e1e0d
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-13 18:52:37 -0300

    create login page

--- Netflix/src/components/Login.jsx
+++ Netflix/src/components/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react"
+import React from "react";
+import PropTypes from 'prop-types';
+import '../css/Login.css';
 
-export class Login extends React.Component {
-  render() {
-    return (<h1>cigarrinhuu</h1>)
-  }
+export function Login({ history }) {
+  const handlePage = () => history.push('/home');
+
+  return (
+    <div>
+      <h1 className="text-zinc-100 text-center mt-[120px] text-[80px]">NETFLIX</h1>
+      <div className="flex flex-col items-center mt-[50px]">
+        <form className="flex flex-col gap-2">
+          <input type="text" placeholder="Digite o Seu Usuário" className="bg-zinc-700 w-[450px] h-[50px] p-4" />
+          <input type="password" placeholder="Digite a Sua Senha" className="bg-zinc-700 h-[50px] p-4" />
+          <button type="submit" onClick={ handlePage } className="flex justify-center items-center m-auto border border-1 border-neutral-900 w-[75px] h-[40px] text-zinc-800 bg-zinc-300 hover:bg-zinc-800 hover:text-zinc-100">Entrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 }
