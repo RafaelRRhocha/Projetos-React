fcacd57c9a0f0d61c71ecd8480e70c84137468c5
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-13 17:28:01 -0300

    initial netflix clone

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+import { Login } from './components/Login';
+import { Home } from './components/Home';
+import { NotFound } from './components/NotFound';
 import './App.css'
 
 function App() {
-  return (<h1>hello</h1>)
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/home" component={ Home } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App
