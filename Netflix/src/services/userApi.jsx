ecd26c1510a0a005fbd71b85765990d86fc99050
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-14 11:36:02 -0300

    rmv loading in login

--- Netflix/src/services/userApi.jsx
+++ Netflix/src/services/userApi.jsx
@@ -3,17 +3,17 @@ const userKey = 'userKey';
 const readUser = () => JSON.parse(localStorage.getItem(userKey));
 const saveUser = (user) => localStorage.setItem(userKey, JSON.stringify(user));
 
-export const createUser = (user) => new Promise(() => {
+export const createUser = (user) => {
   const emptyUser = {
     name: '',
     password: '',
   };
   saveUser({ ...emptyUser, ...user });
-});
+};
 
-export const getUser = () => new Promise(() => {
+export const getUser = () => {
   let user = readUser();
   if (user === null) {
     user = {};
   }
-});
+};
