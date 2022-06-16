334f1e5eb4c408c8518c5a781cd172c4c33d80ee
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-16 13:08:26 -0300

    rmv netflix

--- Netflix/src/components/subComponents/Loading.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export function Loading() {
-  return <p className="text-zinc-100 mt-[10%] text-center text-[45px]">Carregando...</p>
-}
--- Netflix/src/services/userApi.jsx
+++
@@ -1,4 +0,0 @@
-const userKey = 'userKey';
-
-export const saveUser = (user) => localStorage.setItem(userKey, JSON.stringify(user));
-export const readUser = () => JSON.parse(localStorage.getItem(userKey));
