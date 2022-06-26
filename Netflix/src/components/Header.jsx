50f1195f7c0e589fce4cc177f818b3f30251caae
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-26 10:01:31 -0300

    add netflix

---
+++ Netflix/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import logo from '../assets/netflix-icon-main.png';
+import profile from '../assets/profileImage.png';
+import '../css/Main.css';
+import { MagnifyingGlass } from "phosphor-react";
+import { readUser } from "../services/userApi";
+
+export class Header extends React.Component {
+
+  tecMain = () => alert('Em Manutenção!');
+
+  render() {
+    const { bgHeader, viewProfile } = this.props;
+    const user = readUser();
+    const allClass = "fixed z-50 top-0 left-0 right-0 h-[70px] flex justify-between";
+    const allClassWithBg = "fixed z-50 top-0 left-0 right-0 h-[70px] flex justify-between changeBg";
+    return (
+      <header className={!bgHeader ? allClass : allClassWithBg}>
+        <img src={logo} alt="imagem de perfil" className="ml-[30px]"/>
+        {bgHeader && (
+          <div className="flex gap-2 items-center">
+            <input type="text" placeholder="Buscar" className="h-6 w-[300px] text-zinc-800 p-1" />
+            <MagnifyingGlass size={20} onClick={ this.tecMain } className="hover:cursor-pointer text-zinc-100"/>
+          </div>
+        )}
+        <div onClick={ viewProfile } className="flex gap-4 items-center mr-[40px]">
+          <img src={!user.profile ? profile : user.profile} alt="imagem de perfil" className="w-[45px] h-[45px] rounded-sm hover:cursor-pointer" />
+          {bgHeader && <p className="underline decoration-1 hover:cursor-pointer text-zinc-100">{user.name}</p>}
+        </div>
+    </header>
+    )
+  }
+}
+
+Header.propTypes =  {
+  name: PropTypes.string.isRequired,
+  bgHeader: PropTypes.bool.isRequired,
+  viewProfile: PropTypes.func.isRequired,
+};
---
+++ Netflix/src/components/Home.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { getHomeList, getMovieInfo } from "../services/moviesApi";
+import { MovieList } from "./MovieList";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { FeatureMovie } from "./FeatureMovie";
+import { Loading } from "./Loading";
+import PropTypes from 'prop-types';
+
+export class Home extends React.Component {
+  state = {
+    movies: [],
+    bgHeader: false,
+    feature: null,
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.loadAll();
+    this.setLoading();
+    window.addEventListener('scroll', () => {
+      if(window.scrollY >= 10) { this.setState({bgHeader: true}) }
+      if(window.scrollY < 10) { this.setState({bgHeader: false}) }
+    })
+  }
+
+  setLoading = async () => {
+    this.setState({loading: true});
+    await getHomeList();
+    this.setState({loading: false});
+  };
+
+  loadAll = async () => {
+    let list = await getHomeList();
+    let featureFilter = list.filter((item) => item.slug === 'trending');
+    let randomFeatureMovie = Math.floor(Math.random() * (featureFilter[0].items.results.length - 1));
+    let chosenMovie = featureFilter[0].items.results[randomFeatureMovie];
+    let finalChosenMoviesWithAllInfos = await getMovieInfo(chosenMovie.id, 'tv');
+    this.setState({ movies: list, feature: finalChosenMoviesWithAllInfos });
+  }
+
+  viewProfile = () => {
+    this.props.history.push('/profile');
+    document.location.reload(true);
+  };
+
+  render() {
+    const { movies, bgHeader, feature, loading } = this.state;
+    return (
+      <>
+        {loading ? <Loading /> : (
+          <>
+            <div>
+              <Header bgHeader={bgHeader} viewProfile={ this.viewProfile } />
+            </div>
+            <div>
+                {feature && <FeatureMovie feature={feature} />}
+                <div className="text-zinc-100">
+                  {movies.map((movies, i) => (
+                    <MovieList key={i} title={movies.title} items={movies.items}/>
+                  ))}
+                </div>
+            </div>
+            <div>
+              <Footer />
+            </div>
+          </>
+        )}
+      </>
+    )
+  }
+}
+
+Home.propTypes =  {
+  history: PropTypes.shape(() => ({
+    push: PropTypes.func.isRequired,
+  }))
+}
